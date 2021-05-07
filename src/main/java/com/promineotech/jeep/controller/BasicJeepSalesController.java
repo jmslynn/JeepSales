c0b1cc606a78a8c63d6199a066dd785014fa520d
jmslynn <72352820+jmslynn@users.noreply.github.com>
2021-05-07 14:31:54 -0400

    Edits after video 3 and mentor meeting

--- src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
+++ src/main/java/com/promineotech/jeep/controller/BasicJeepSalesController.java
@@ -2,19 +2,24 @@ package com.promineotech.jeep.controller;
 
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
+import com.promineotech.jeep.service.JeepSalesService;
 
 @RestController
 @Slf4j
 public class BasicJeepSalesController implements JeepSalesController {
 
+	@Autowired
+	private JeepSalesService jeepSalesService;
 
 	@Override
 	public List<Jeep> fetchJeeps(JeepModel model, String trim) {
 		log.debug ("model= {}, trim= {}", model, trim);
-		return null;
+		return jeepSalesService.fetchJeeps(model, trim);
 	}
 
 }
--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -1,37 +1,58 @@
-package com.promineotech.jeep.controller;
+ package com.promineotech.jeep.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+import net.bytebuddy.NamingStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.core.ParameterizedTypeReference;
 
-import com.promineotech.jeep.JeepSales;
 import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
-import io.swagger.v3.oas.models.PathItem.HttpMethod;
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import java.util.List;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.test.context.ActiveProfiles;
+//import org.springframework.test.context.jdbc.Sql;
+//import org.springframework.test.context.jdbc.SqlConfig;
+//import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+//import org.springframework.core.ParameterizedTypeReference;
+//
+//import com.promineotech.jeep.JeepSales;
+//import com.promineotech.jeep.controller.support.FetchJeepTestSupport;
+//import com.promineotech.jeep.entity.Jeep;
+//import com.promineotech.jeep.entity.JeepModel;
+//
+//import io.swagger.v3.oas.models.PathItem.HttpMethod;
 
 
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes= {JeepSales.class})
-//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+//@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes= {JeepSales.class})
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@Sql(scripts = {
-	    "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
-	    "classpath:flyway/migrations/V1.1__Jeep_Data.sql"}, 
-	    config = @SqlConfig(encoding = "utf-8"))
+//@Sql(scripts = {
+//	    "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+//	    "classpath:flyway/migrations/V1.1__Jeep_Data.sql"}, 
+//	    config = @SqlConfig(encoding = "utf-8"))
 
 
 class FetchJeepTest extends FetchJeepTestSupport {
@@ -45,7 +66,6 @@ class FetchJeepTest extends FetchJeepTestSupport {
 		
 		
 		//when: a connection is made to the uri
-		//per video
 		ResponseEntity<List<Jeep>> response = getRestTemplate().exchange(uri, HttpMethod.GET, null, 
 				new ParameterizedTypeReference<>() {});
 	
